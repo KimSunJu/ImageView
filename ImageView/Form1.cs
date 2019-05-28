9c62bb54b5cf9875f4bec15692a14dc98bd08056
Sunju Kim <sunju7420@naver.com>
2019-05-28 13:14:33 +0900

    폼 디자인

--- ImageView/Form1.cs
+++ ImageView/Form1.cs
@@ -11,9 +11,30 @@ namespace ImageView
 {
     public partial class Form1 : Form
     {
+        int ImgCount = 0; // 현재 보고있는 이미지 번호
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.picImg.Image = (Image)this.imgList.Images[this.ImgCount];
+            this.lblNum.Text = (this.ImgCount + 1) + "/" + this.imgList.Images.Count;
+
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            this.ImgCount--;
+            if (this.ImgCount < 0)
+            {
+                this.ImgCount = this.imgList.Images.Count - 1;
+            }
+            this.picImg.Image = (Image)this.imgList.Images[this.ImgCount];
+            this.lblNum.Text = (this.ImgCount + 1)
+                + "/" + this.imgList.Images.Count;
+
         }
     }
 }
